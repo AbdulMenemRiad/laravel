a7998291c07800c8875754595ff96a7f73d90beb
Abdul menem <abdulmenemriad@gmail.com>
2024-05-03 00:32:07 +0400

    getting a 404 error for a sinle listing

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,15 +1,18 @@
 <h1>{{$heading}}</h1>
 
-@if(count($listings) == 0)
-  <p>No listings found</P>
-@endif
+@unless(count($listings) == 0)
 
 @foreach($listings as $listing)
 <h2> 
-  {{$listing['title']}}
+  <a href="/listings/{{$listing['id']}}">{{$listing
+  ['title']}} </a>
 </h2>
 <p>
   {{$listing['description']}}
 </p>
 @endforeach
 
+    
+@else
+<p>No listings found</p>
+@endunless
